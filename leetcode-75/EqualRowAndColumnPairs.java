4f889909d01b761960f9bec4138d69907f8193be
devdogukan <dgnkaksy06@gmail.com>
2025-04-08 09:37:16 +0300

    Refactor equalPairs method to use string keys for row and column counting, improving readability and efficiency

--- leetcode-75/EqualRowAndColumnPairs.java
+++ leetcode-75/EqualRowAndColumnPairs.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class EqualRowAndColumnPairs {
@@ -8,49 +6,31 @@ public class EqualRowAndColumnPairs {
     public static void main(String[] args) {
         System.out.println(equalPairs(new int[][] { { 3, 2, 1 }, { 1, 7, 6 }, { 2, 7, 7 } }));
         System.out.println(equalPairs(new int[][] { { 3, 1, 2, 2 }, { 1, 4, 4, 5 }, { 2, 4, 2, 2 }, { 2, 4, 2, 2 } }));
+        System.out.println(equalPairs(new int[][]  { { 11, 1 }, { 1, 11 } }));
     }
 
     public static int equalPairs(int[][] grid) {
-
-        Map<List<Integer>, Integer> rows = new HashMap<>();
-        Map<List<Integer>, Integer> columns = new HashMap<>();
-
-        for (int[] row : grid) {
-            List<Integer> rowList = new ArrayList<>();
-            for (int num : row) {
-                rowList.add(num);
+        int n = grid.length;
+        Map<String, Integer> rowCount = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                sb.append(grid[i][j]).append(",");
             }
-
-            rows.put(rowList, rows.getOrDefault(rowList, 0) + 1);
+            String rowKey = sb.toString();
+            rowCount.put(rowKey, rowCount.getOrDefault(rowKey, 0) + 1);
         }
-
-        int rowCount = grid.length;
-        int colCount = grid[0].length;
-
-        for (int col = 0; col < colCount; col++) {
-            List<Integer> columnList = new ArrayList<>();
-            for (int row = 0; row < rowCount; row++) {
-                columnList.add(grid[row][col]);
-            }
-
-            columns.put(columnList, columns.getOrDefault(columnList, 0) + 1);
-        }
-
 
         int count = 0;
-        for (Map.Entry<List<Integer>, Integer> rowEntry : rows.entrySet()) {
-            List<Integer> rowKey = rowEntry.getKey();
-            Integer rowValue = rowEntry.getValue();
-        
-            if (columns.containsKey(rowKey)) {
-                Integer columnValue = columns.get(rowKey);
-                int sum = rowValue * columnValue;
-                
-                count += sum;
+        for (int j = 0; j < n; j++) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                sb.append(grid[i][j]).append(",");
             }
+            String colKey = sb.toString();
+            count += rowCount.getOrDefault(colKey, 0);
         }
-        
 
-        return count;
+        return count;   
     }
 }
